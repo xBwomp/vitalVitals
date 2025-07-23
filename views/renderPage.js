5f78227e41ec210e298be85b7e222d3089206f89
Jeff <821659+xBwomp@users.noreply.github.com>
2025-07-22 21:45:20 -0400

    escape user display name

--- views/renderPage.js
+++ views/renderPage.js
@@ -3,10 +3,19 @@ const path = require('path');
 
 const layoutTemplate = fs.readFileSync(path.join(__dirname, 'layout.html'), 'utf8');
 
+function escapeHtml(str) {
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+}
+
 module.exports = function renderPage(content, user) {
     const isLoggedIn = !!user;
     const authLinks = isLoggedIn
-        ? `Welcome, ${user.displayName} | <a href="/vitals/logout">Log out</a>`
+        ? `Welcome, ${escapeHtml(user.displayName)} | <a href="/vitals/logout">Log out</a>`
         : `<a href="/vitals/auth/google">Log in with Google</a>`;
 
     return layoutTemplate
